be2dc710d89da9466feba53acd0cdbca4da254f9
Yihe Li <winmikedows@hotmail.com>
2023-12-19 13:07:55 +0800

    Fix station_time for loop lines

--- src/routing/show_station_time.py
+++ src/routing/show_station_time.py
@@ -8,25 +8,30 @@ import os
 import sys
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 from common.common import get_time_str, diff_time
-from city.ask_for_city import ask_for_city, ask_for_line, ask_for_station_pair_in_line,\
-    ask_for_date_group
+from city.ask_for_city import ask_for_city, ask_for_line, ask_for_direction,\
+    ask_for_station_pair_in_line, ask_for_date_group
 from city.date_group import DateGroup
 from city.line import Line
 from routing.train import parse_trains
 
 def get_time_between(
-    line: Line, date_group: DateGroup, start: str, end: str
+    line: Line, date_group: DateGroup, start: str, end: str,
+    *, with_direction: str | None = None
 ) -> tuple[str, dict[str, int | None]]:
     """ Get time between two stations """
     # First determine the direction
+    assert not (line.loop and with_direction is None), line
     assert start != end, (start, end)
-    for direction, direction_stations in line.directions.items():
-        start_index = direction_stations.index(start)
-        end_index = direction_stations.index(end)
-        if start_index >= 0 and end_index >= 0 and start_index < end_index:
-            break
+    if with_direction is None:
+        for direction, direction_stations in line.directions.items():
+            start_index = direction_stations.index(start)
+            end_index = direction_stations.index(end)
+            if start_index >= 0 and end_index >= 0 and start_index < end_index:
+                break
+        else:
+            assert False, (line, start, end)
     else:
-        assert False, (line, start, end)
+        direction = with_direction
 
     # calculate time for each train
     train_dict = parse_trains(line, set([direction]))
@@ -38,19 +43,34 @@ def get_time_between(
         time_str = get_time_str(*train.arrival_time[start])
         if end not in train.arrival_time:
             time_dict[time_str] = None
+            continue
+        arrival_keys = list(train.arrival_time.keys())
+        start_index = arrival_keys.index(start)
+        end_index = arrival_keys.index(end)
+        start_time, start_day = train.arrival_time[start]
+        if end_index < start_index:
+            assert line.loop, line
+            if train.loop_next is None:
+                time_dict[time_str] = None
+                continue
+            end_time, end_day = train.loop_next.arrival_time[end]
         else:
-            start_time, start_day = train.arrival_time[start]
             end_time, end_day = train.arrival_time[end]
-            time_dict[time_str] = diff_time(end_time, start_time, end_day, start_day)
+        time_dict[time_str] = diff_time(end_time, start_time, end_day, start_day)
     return direction, time_dict
 
 def main() -> None:
     """ Main function """
     city = ask_for_city()
     line = ask_for_line(city)
+    if line.loop:
+        with_direction = ask_for_direction(line)
+    else:
+        with_direction = None
     start, end = ask_for_station_pair_in_line(line, with_timetable=True)
     date_group = ask_for_date_group(line)
-    direction, time_dict = get_time_between(line, date_group, start, end)
+    direction, time_dict = get_time_between(
+        line, date_group, start, end, with_direction=with_direction)
     line.timetables()[start][direction][date_group.name].pretty_print(with_time=time_dict)
 
 # Call main
