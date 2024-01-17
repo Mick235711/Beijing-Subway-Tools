044404713c21d0f67da9d80be00f5cd7f7d3973a
Yihe Li <winmikedows@hotmail.com>
2024-01-17 17:46:45 +0800

    Add per-minute train count

--- src/stats/hour_trains.py
+++ src/stats/hour_trains.py
@@ -4,6 +4,10 @@
 """ Print train count by hour """
 
 # Libraries
+import argparse
+import json
+
+from src.common.common import add_min, get_time_str
 from src.routing.train import Train
 from src.stats.city_statistics import display_first, divide_by_line, parse_args
 
@@ -26,10 +30,41 @@ def hour_trains(all_trains: dict[str, list[tuple[str, Train]]]) -> None:
     )
 
 
+def minute_trains(all_trains: dict[str, list[tuple[str, Train]]]) -> dict[str, dict[str, int]]:
+    """ Print train number per minute """
+    minute_dict: dict[str, dict[str, int]] = {"Total": {}}
+    for date_group, train in set(t for x in all_trains.values() for t in x):
+        if train.line.name not in minute_dict:
+            minute_dict[train.line.name] = {}
+        start_tuple = train.arrival_time[train.stations[0]]
+        for i in range(0, train.duration() + 1):
+            cur_tuple = add_min(start_tuple[0], i, start_tuple[1])
+            cur_str = get_time_str(*cur_tuple)
+            if cur_str not in minute_dict[train.line.name]:
+                minute_dict[train.line.name][cur_str] = 0
+            minute_dict[train.line.name][cur_str] += 1
+            if cur_str not in minute_dict["Total"]:
+                minute_dict["Total"][cur_str] = 0
+            minute_dict["Total"][cur_str] += 1
+    for line, line_dict in minute_dict.items():
+        minute_dict[line] = dict(sorted(line_dict.items(), key=lambda x: x[0]))
+    return dict(sorted(minute_dict.items(), key=lambda x: x[0]))
+
+
 def main() -> None:
     """ Main function """
-    all_trains, _ = parse_args(include_limit=False)
-    hour_trains(all_trains)
+    def append_arg(parser: argparse.ArgumentParser) -> None:
+        """ Append more arguments """
+        parser.add_argument("-m", "--by-minutes", action="store_true", help="Output data by minutes")
+        parser.add_argument("-o", "--output", help="Output path", default="../data.json")
+
+    all_trains, args = parse_args(append_arg, include_limit=False)
+    if args.by_minutes:
+        data = minute_trains(all_trains)
+        with open(args.output, "w", encoding="utf-8") as fp:
+            json.dump(data, fp, indent=4, ensure_ascii=False)
+    else:
+        hour_trains(all_trains)
 
 
 # Call main
