3d4ccf0fa7c7ebab6a5c6fc30fa4a096d5942033
Yihe Li <winmikedows@hotmail.com>
2024-02-16 15:01:55 +0800

    Add -c to max_train_station

--- src/stats/max_train_station.py
+++ src/stats/max_train_station.py
@@ -4,26 +4,37 @@
 """ Print stations with max number of train/capacity """
 
 # Libraries
+import argparse
+
+from src.common.common import suffix_s
 from src.routing.train import Train
 from src.stats.city_statistics import display_first, divide_by_line, parse_args
 
 
 def max_train_station(
-    all_trains: dict[str, list[tuple[str, Train]]], *, limit_num: int = 5
+    all_trains: dict[str, list[tuple[str, Train]]], *, limit_num: int = 5, use_capacity: bool = False
 ) -> None:
     """ Print max/min # of trains for each station """
     display_first(
-        all_trains.items(),
-        lambda station_trains: f"{station_trains[0]}: {len(station_trains[1])} trains " +
-                               f"({divide_by_line(x[1] for x in station_trains[1])})",
+        sorted(all_trains.items(), key=lambda x: (
+            sum(t.train_capacity() for _, t in x[1]) if use_capacity else len(x[1])
+        ), reverse=True),
+        lambda station_trains: f"{station_trains[0]}: " + (
+            suffix_s("people", sum(t.train_capacity() for _, t in station_trains[1])) if use_capacity else
+            suffix_s("train", len(station_trains[1]))
+        ) + f" ({divide_by_line([x[1] for x in station_trains[1]], use_capacity=use_capacity)})",
         limit_num=limit_num
     )
 
 
 def main() -> None:
     """ Main function """
-    all_trains, _, _, args = parse_args()
-    max_train_station(all_trains, limit_num=args.limit_num)
+    def append_arg(parser: argparse.ArgumentParser) -> None:
+        """ Append more arguments """
+        parser.add_argument("-c", "--capacity", action="store_true", help="Output capacity data")
+
+    all_trains, _, _, args = parse_args(append_arg)
+    max_train_station(all_trains, limit_num=args.limit_num, use_capacity=args.capacity)
 
 
 # Call main
